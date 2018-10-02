d374c633c57d9f798996f1e8d438a2fc3812e1b8
Kaira Wong <wongtsetse@gmail.com>
2018-10-02 14:17:09 +1000

    Added in toolbar at the top

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title>In the Moment</title>
 	
@@ -7,10 +8,27 @@
 	<link rel="stylesheet" href="https://unpkg.com/onsenui/css/onsen-css-components.min.css">
 	<script src="https://unpkg.com/onsenui/js/onsenui.min.js"></script>
 </head>
+
 <body>
 
-	<h1>Hello World</h1>
-	<h2>In the Moment</h2>
+	<ons-page>
+		<ons-toolbar>
+		  <div class="left">
+		    Left part
+		  </div>
+		  <div class="center">
+		    Title here
+		  </div>
+		  <div class="right">
+		    Right part
+		  </div>
+		</ons-toolbar>
+
+		<h1>Hello World</h1>
+		<h2>In the Moment</h2>
+		<ons-button onclick="alert('You clicked me!')">Click Me</ons-button>
+	</ons-page>
 	
 </body>
+
 </html>
