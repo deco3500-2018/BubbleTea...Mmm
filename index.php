5a03c8daff0e84673d2f459c771c1f92bc53d249
Kaira Wong <wongtsetse@gmail.com>
2018-10-02 13:35:14 +1000

    Created index page

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>In the Moment</title>
+	
+	<link rel="stylesheet" href="https://unpkg.com/onsenui/css/onsenui.css">
+	<link rel="stylesheet" href="https://unpkg.com/onsenui/css/onsen-css-components.min.css">
+	<script src="https://unpkg.com/onsenui/js/onsenui.min.js"></script>
+</head>
+<body>
+
+</body>
+</html>
