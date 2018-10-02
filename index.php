0a1550d4814fcae7bc95f6cf630eaa2f6e7e2e68
Kaira Wong <wongtsetse@gmail.com>
2018-10-02 14:34:30 +1000

    Basic layout (incl. generic footer bar and toolbar)

--- index.php
+++ index.php
@@ -11,68 +11,85 @@
 
 <body>
 
-<ons-page>
+	<ons-page>
 
-  	<ons-toolbar>
-		  <div class="left">
-		    Left part
-		  </div>
-		  <div class="center">
-		    Title here
-		  </div>
-		  <div class="right">
-		    Right part
-		  </div>
+		<ons-toolbar>
+			<div class="left">
+				<ons-toolbar-button icon="md-face"></ons-toolbar-button>
+			</div>
+
+			<div class="center">Title</div>
+
+			<div class="right">
+				<ons-toolbar-button>-</ons-toolbar-button>
+				<ons-toolbar-button>+</ons-toolbar-button>
+			</div>
 		</ons-toolbar>
-		
-    <p class="intro">
-      This is a kitchen sink example that shows off the components of Onsen UI.<br><br>
-    </p>
 
-    <ons-card onclick="fn.pushPage({'id': 'pullHook.html', 'title': 'PullHook'})">
-      <div class="title">Pull Hook</div>
-      <div class="content">Simple "pull to refresh" functionality to update data.</div>
-    </ons-card>
-    <ons-card onclick="fn.pushPage({'id': 'dialogs.html', 'title': 'Dialogs'})">
-      <div class="title">Dialogs</div>
-      <div class="content">Components and utility methods to display many types of dialogs.</div>
-    </ons-card>
-    <ons-card onclick="fn.pushPage({'id': 'buttons.html', 'title': 'Buttons'})">
-      <div class="title">Buttons</div>
-      <div class="content">Different styles for buttons, floating action buttons and speed dials.</div>
-    </ons-card>
-    <ons-card onclick="fn.pushPage({'id': 'carousel.html', 'title': 'Carousel'})">
-      <div class="title">Carousel</div>
-      <div class="content">Customizable carousel that can be optionally fullscreen.</div>
-    </ons-card>
-    <ons-card onclick="fn.pushPage({'id': 'infiniteScroll.html', 'title': 'Infinite Scroll'})">
-      <div class="title">Infinite Scroll</div>
-      <div class="content">Two types of infinite lists: "Load More" and "Lazy Repeat".</div>
-    </ons-card>
-    <ons-card onclick="fn.pushPage({'id': 'progress.html', 'title': 'Progress'})">
-      <div class="title">Progress</div>
-      <div class="content">Linear progress, circular progress and spinners.</div>
-    </ons-card>
+		<p class="intro">
+			This is a kitchen sink example that shows off the components of Onsen UI.<br><br>
+		</p>
 
-    <style>
-      .intro {
-        text-align: center;
-        padding: 0 20px;
-        margin-top: 40px;
-      }
+		<ons-card onclick="fn.pushPage({'id': 'pullHook.html', 'title': 'PullHook'})">
+			<div class="title">Pull Hook</div>
+			<div class="content">Simple "pull to refresh" functionality to update data.</div>
+		</ons-card>
+		<ons-card onclick="fn.pushPage({'id': 'dialogs.html', 'title': 'Dialogs'})">
+			<div class="title">Dialogs</div>
+			<div class="content">Components and utility methods to display many types of dialogs.</div>
+		</ons-card>
+		<ons-card onclick="fn.pushPage({'id': 'buttons.html', 'title': 'Buttons'})">
+			<div class="title">Buttons</div>
+			<div class="content">Different styles for buttons, floating action buttons and speed dials.</div>
+		</ons-card>
+		<ons-card onclick="fn.pushPage({'id': 'carousel.html', 'title': 'Carousel'})">
+			<div class="title">Carousel</div>
+			<div class="content">Customizable carousel that can be optionally fullscreen.</div>
+		</ons-card>
+		<ons-card onclick="fn.pushPage({'id': 'infiniteScroll.html', 'title': 'Infinite Scroll'})">
+			<div class="title">Infinite Scroll</div>
+			<div class="content">Two types of infinite lists: "Load More" and "Lazy Repeat".</div>
+		</ons-card>
+		<ons-card onclick="fn.pushPage({'id': 'progress.html', 'title': 'Progress'})">
+			<div class="title">Progress</div>
+			<div class="content">Linear progress, circular progress and spinners.</div>
+		</ons-card>
 
-      ons-card {
-        cursor: pointer;
-        color: #333;
-      }
+		<style>
+		.intro {
+			text-align: center;
+			padding: 0 20px;
+			margin-top: 40px;
+		}
 
-      .card__title,
-      .card--material__title {
-        font-size: 20px;
-      }
-    </style>
-  </ons-page>
-	
+		ons-card {
+			cursor: pointer;
+			color: #333;
+		}
+
+		.card__title,
+		.card--material__title {
+			font-size: 20px;
+		}
+	</style>
+
+
+	<ons-bottom-toolbar>
+		<ons-tabbar swipeable position="auto">
+			<ons-tab page="tab1.html" label="Tab 1" icon="ion-home, material:md-home" badge="7" active>
+			</ons-tab>
+			<ons-tab page="tab2.html" label="Tab 2" icon="md-settings" active-icon="md-face">
+			</ons-tab>
+			<ons-tab page="tab3.html" label="Tab 3" icon="md-settings" active-icon="md-face">
+			</ons-tab>
+			<ons-tab page="tab2.html" label="Tab 4" icon="md-settings" active-icon="md-face">
+			</ons-tab>
+		</ons-tabbar>
+	</ons-bottom-toolbar>
+
+
+</ons-page>
+
 </body>
 
 </html>
