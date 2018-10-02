344ed48f5373c4f6e2eae49d1c00a0e81bd2a80b
Kaira Wong <wongtsetse@gmail.com>
2018-10-02 14:24:39 +1000

    Test

--- index.php
+++ index.php
@@ -11,8 +11,9 @@
 
 <body>
 
-	<ons-page>
-		<ons-toolbar>
+<ons-page>
+
+  	<ons-toolbar>
 		  <div class="left">
 		    Left part
 		  </div>
@@ -23,11 +24,54 @@
 		    Right part
 		  </div>
 		</ons-toolbar>
+		
+    <p class="intro">
+      This is a kitchen sink example that shows off the components of Onsen UI.<br><br>
+    </p>
 
-		<h1>Hello World</h1>
-		<h2>In the Moment</h2>
-		<ons-button onclick="alert('You clicked me!')">Click Me</ons-button>
-	</ons-page>
+    <ons-card onclick="fn.pushPage({'id': 'pullHook.html', 'title': 'PullHook'})">
+      <div class="title">Pull Hook</div>
+      <div class="content">Simple "pull to refresh" functionality to update data.</div>
+    </ons-card>
+    <ons-card onclick="fn.pushPage({'id': 'dialogs.html', 'title': 'Dialogs'})">
+      <div class="title">Dialogs</div>
+      <div class="content">Components and utility methods to display many types of dialogs.</div>
+    </ons-card>
+    <ons-card onclick="fn.pushPage({'id': 'buttons.html', 'title': 'Buttons'})">
+      <div class="title">Buttons</div>
+      <div class="content">Different styles for buttons, floating action buttons and speed dials.</div>
+    </ons-card>
+    <ons-card onclick="fn.pushPage({'id': 'carousel.html', 'title': 'Carousel'})">
+      <div class="title">Carousel</div>
+      <div class="content">Customizable carousel that can be optionally fullscreen.</div>
+    </ons-card>
+    <ons-card onclick="fn.pushPage({'id': 'infiniteScroll.html', 'title': 'Infinite Scroll'})">
+      <div class="title">Infinite Scroll</div>
+      <div class="content">Two types of infinite lists: "Load More" and "Lazy Repeat".</div>
+    </ons-card>
+    <ons-card onclick="fn.pushPage({'id': 'progress.html', 'title': 'Progress'})">
+      <div class="title">Progress</div>
+      <div class="content">Linear progress, circular progress and spinners.</div>
+    </ons-card>
+
+    <style>
+      .intro {
+        text-align: center;
+        padding: 0 20px;
+        margin-top: 40px;
+      }
+
+      ons-card {
+        cursor: pointer;
+        color: #333;
+      }
+
+      .card__title,
+      .card--material__title {
+        font-size: 20px;
+      }
+    </style>
+  </ons-page>
 	
 </body>
 
