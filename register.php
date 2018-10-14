fa820a15a7051d8bc07ebfc1837b22912e96402e
Kaira Wong <wongtsetse@gmail.com>
2018-10-14 13:50:49 +1000

    updated interface of register page to new design. currently only basic layout implemented

--- register.php
+++ register.php
@@ -17,40 +17,40 @@ if(isset($_POST['register_btn']))
     }
 
     else if($password==$password2)
-     {
-		
+    {
+      
 		//Create User
         
-             $hash = password_hash($password, PASSWORD_DEFAULT);
+       $hash = password_hash($password, PASSWORD_DEFAULT);
 
            // $password=md5($password); //hash password before storing for security purposes
-            
-            $sql="INSERT INTO users(username,email,password) VALUES('$username','$email','$hash')";
-            mysqli_query($db,$sql);
+       
+       $sql="INSERT INTO users(username,email,password) VALUES('$username','$email','$hash')";
+       mysqli_query($db,$sql);
 
-            $sql="SELECT * FROM users WHERE username='$username' AND password='$hash'";
-            $result=mysqli_query($db,$sql);
-            $user = mysqli_fetch_assoc($result);
+       $sql="SELECT * FROM users WHERE username='$username' AND password='$hash'";
+       $result=mysqli_query($db,$sql);
+       $user = mysqli_fetch_assoc($result);
 
-           
-            $_SESSION['username']=$username;
-            $_SESSION['user'] = $user;
-            $_SESSION['loggedin']=true;
-            header("location:index.php");  
-    }
-    else
-    {
+       
+       $_SESSION['username']=$username;
+       $_SESSION['user'] = $user;
+       $_SESSION['loggedin']=true;
+       header("location:index.php");  
+   }
+   else
+   {
       $_SESSION['message']="The two password do not match";
-     }
+  }
 
 }
-     if($_SERVER["REQUEST_METHOD"] == "POST"){
+if($_SERVER["REQUEST_METHOD"] == "POST"){
 
     // Validate username
     if(empty(trim($_POST["username"]))){
         $username_err = "Please enter a username.";
     }
-     }
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -65,136 +65,93 @@ if(isset($_POST['register_btn']))
 
 <body>
 
-	<ons-page>
+	<ons-page id="login-bg">
 
-		<ons-toolbar>
-			<div class="left">
-				<ons-toolbar-button icon="md-account-circle"></ons-toolbar-button>
-			</div>
+        <style type="text/css">
+        #login-bg .page__background {
+            background-image: url('img/grad.png');
+            object-fit: cover;
+            background-repeat: no-repeat;
+        }
 
-			<div class="center">Title</div>
-			
-			
-			
+        #textclr{
+            color: white;
+        }
 
-			<div class="right">
-				<ons-toolbar-button>-</ons-toolbar-button>
-				<ons-toolbar-button>+</ons-toolbar-button>
-			</div>
-		</ons-toolbar>
-		
-		<!-- start of input form -->
-		
-		<form method="post" action="register.php" id="frm_register" style="margin: auto;">
-  <div id="register_message" style="text-align: center;"></div>
-  <table style="text-align: center; margin:auto;">
-     <tr>
-      <td>Name </td>
-       <td>
-         <input type="text" name="Name" class="textInput form-control" id="txt_username" placeholder="John" required style="margin-left: 10px;">
-       </td>
-     </tr>
+        #txtbold{
+            color: white;
+            font-weight: bold;
+        }
 
-     <tr style="margin: 10px;">
-       <td>Email </td>
-       <td style="text-align: left"><input id="txt_email" type="email" name="email" class="textInput form-control" required="required" placeholder="john123@gmail.com" style="margin-left: 10px;"></td>
-	   <div id="uname_response" class="response"></div>
-     </tr>
+    </style>
 
-     <tr style="margin-top: 10px;">
-       <td>Password </td>
-       <td><input id="txt_password" type="password" name="password" class="textInput form-control" required="required" style="margin-left: 10px;"></td>
 
-     </tr>
-     <tr>
-       <td>Confirm Password </td>
-       <td><input id="txt_repassword" type="password" name="password2" class="textInput form-control" required style="margin-left: 10px;"></td>
-     </tr>
-     <tr>
-       <td></td>
-       <td>
-        <br>
-        <div style= "text-align:center">
-          <input type="submit" id="btn_submit" style="display:none;"/>
-          <button type="button" class="Register" id="btn_register" style="border-radius:14px; color:white; font-size:15px; background-color:#008CBA; padding:10px; text-align:center; width: 200px;">Register</button>
-          <br>
-          <br>
-          <p>Already a member? <a href="login.php"> Login</a></p>
-        </td>
+    <!-- Navbar -->
+    <ons-toolbar class="toolbar toolbar--transparent">
+        <div class="toolbar__right">
+            <span class="toolbar-button">
+                <i class="ion-navicon" style="font-size:32px; vertical-align:-6px; margin-right: 10px;" id="textclr"></i>
+            </span>
+        </div>
+
+    </ons-toolbar>
+
+    <!-- start of input form -->
+
+    <form method="post" action="register.php" id="frm_register" style="margin: auto;">
+      <div id="register_message" style="text-align: center;">
+          <h1 id="textclr" style="text-align: center; margin-top: 10%;">Register</h1>
       </div>
+      <table style="text-align: left; margin:auto;" id="txtbold">
+       <tr>
+          <td>Name </td>
+          <td>
+           <!-- <input type="text" name="Name" class="textInput form-control" id="txt_username" placeholder="John" required style="margin-left: 10px;"> -->
+           <ons-input input-id="textclr" modifier="underbar" placeholder="Username" type="text" name="Name" class="textInput form-control" id="txt_username" required></ons-input>
+       </td>
+   </tr>
 
-    </tr>
+   <tr>
+     <td>Email </td>
+     <td style="text-align: left">
+        <ons-input input-id="textclr" modifier="underbar" type="email" name="email" class="textInput form-control" required="required" placeholder="john123@gmail.com" id="txt_email" required></ons-input>
+    </td>
+    <div id="uname_response" class="response"></div>
+</tr>
+
+<tr>
+ <td>Password </td>
+ <td>
+    <!-- <input id="txt_password" type="password" name="password" class="textInput form-control" required="required" style="margin-left: 10px;"> -->
+    <ons-input input-id="textclr" modifier="underbar" type="password" name="password" class="textInput form-control" required="required" id="txt_password" required></ons-input>
+</td>
+
+</tr>
+<tr>
+ <td>Confirm Password </td>
+ <td>
+    <!-- <input id="txt_repassword" type="password" name="password2" class="textInput form-control" required style="margin-left: 10px;"> -->
+    <ons-input input-id="textclr" modifier="underbar" type="password" name="password2" class="textInput form-control" required="required" id="txt_repassword" required></ons-input>
+</td>
+</tr>
 
 
-  </table>
+</table>
+
+<br><br>
+<div style= "text-align:center; margin:auto;">
+  <input type="submit" id="btn_submit" style="display:none;"/>      
+  <ons-button type="button" class="Register button button--cta" id="btn_register">Register</ons-button>
+  <br>
+  <br>
+  <p id="textclr">Already a member? <a href="login.php"> Login</a></p>
+</div>
 </form>
-		
-		<!-- end of input form -->
 
-		<p class="intro">
-			This is a kitchen sink example that shows off the components of Onsen UI.<br><br>
-		</p>
-
-		<ons-card onclick="fn.pushPage({'id': 'pullHook.html', 'title': 'PullHook'})">
-			<div class="title">Pull Hook</div>
-			<div class="content">Simple "pull to refresh" functionality to update data.</div>
-		</ons-card>
-		<ons-card onclick="fn.pushPage({'id': 'dialogs.html', 'title': 'Dialogs'})">
-			<div class="title">Dialogs</div>
-			<div class="content">Components and utility methods to display many types of dialogs.</div>
-		</ons-card>
-		<ons-card onclick="fn.pushPage({'id': 'buttons.html', 'title': 'Buttons'})">
-			<div class="title">Buttons</div>
-			<div class="content">Different styles for buttons, floating action buttons and speed dials.</div>
-		</ons-card>
-		<ons-card onclick="fn.pushPage({'id': 'carousel.html', 'title': 'Carousel'})">
-			<div class="title">Carousel</div>
-			<div class="content">Customizable carousel that can be optionally fullscreen.</div>
-		</ons-card>
-		<ons-card onclick="fn.pushPage({'id': 'infiniteScroll.html', 'title': 'Infinite Scroll'})">
-			<div class="title">Infinite Scroll</div>
-			<div class="content">Two types of infinite lists: "Load More" and "Lazy Repeat".</div>
-		</ons-card>
-		<ons-card onclick="fn.pushPage({'id': 'progress.html', 'title': 'Progress'})">
-			<div class="title">Progress</div>
-			<div class="content">Linear progress, circular progress and spinners.</div>
-		</ons-card>
-
-		<style>
-		.intro {
-			text-align: center;
-			padding: 0 20px;
-			margin-top: 40px;
-		}
-
-		ons-card {
-			cursor: pointer;
-			color: #333;
-		}
-
-		.card__title,
-		.card--material__title {
-			font-size: 20px;
-		}
-	</style>
-
-
-	<ons-bottom-toolbar>
-		<ons-tabbar swipeable position="auto">
-			<ons-tab page="tab1.html" label="Tab 1" icon="ion-home, material:md-home" badge="7" active>
-			</ons-tab>
-			<ons-tab page="tab2.html" label="Tab 2" icon="md-settings" active-icon="md-face">
-			</ons-tab>
-			<ons-tab page="tab3.html" label="Tab 3" icon="md-settings" active-icon="md-face">
-			</ons-tab>
-			<ons-tab page="tab2.html" label="Tab 4" icon="md-settings" active-icon="md-face">
-			</ons-tab>
-		</ons-tabbar>
-	</ons-bottom-toolbar>
-
+<!-- end of input form -->
 
 </ons-page>
-		
+
 
 </body>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
@@ -241,10 +198,10 @@ if(isset($_POST['register_btn']))
                 },
                 error: function() {
                    // alert("Unable to register");
-					 window.location.href="index.php"
-					
-                }
-            })
+                   window.location.href="index.php"
+                   
+               }
+           })
         });
 
         $("#txt_email").on('blur', function(){
