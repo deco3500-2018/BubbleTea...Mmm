4effd53ed3ba2efa3f8e9ef9b55447742033fba4
Kaira Wong <wongtsetse@gmail.com>
2018-10-14 21:31:11 +1000

    Created basic page for showing activity recommendation.

---
+++ activityRec.php
@@ -0,0 +1,148 @@
+<?php
+include "start.php";
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>In the Moment</title>
+	
+	<link rel="stylesheet" href="https://unpkg.com/onsenui/css/onsenui.css">
+	<link rel="stylesheet" href="https://unpkg.com/onsenui/css/onsen-css-components.min.css">
+	<script src="https://unpkg.com/onsenui/js/onsenui.min.js"></script>
+	<script src="https://d3js.org/d3.v5.min.js"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.js"></script>
+</head>
+
+<body>
+
+	<ons-page id="bg">
+		<div class="background" style="background-image: url('img/grad.png');"></div>
+
+		<style type="text/css">
+
+		#bg .page__background {
+			background-color: white;
+		}
+
+		#textclr{
+			color: white;
+		}
+
+		#white{
+			color: white;
+		}
+
+	</style>
+
+
+	<!-- Navbar -->
+	<ons-toolbar class="toolbar toolbar--transparent">
+		<div class="toolbar__right">
+			<span class="toolbar-button">
+				<i class="ion-navicon" style="font-size:32px; vertical-align:-6px; margin-right: 10px; color: white;"></i>
+			</span>
+		</div>
+
+		<div class="toolbar__center" id="textclr" style="text-transform: bold;">
+			Recommended Activity
+		</div>
+
+		<div class="toolbar__left">
+			<ons-toolbar-button icon="md-face" style="margin-left: 10px; color: white;"></ons-toolbar-button>
+		</div>
+	</ons-toolbar>
+
+
+		<ons-card>
+			<img src="img/card.jpg" alt="Onsen UI" style="width: 100%">
+			<div class="title" style="text-align: center;">
+				Make a dessert in under 20 minutes!
+			</div>
+			<br>
+			<div class="content">
+
+				<ons-row style="margin-bottom: 15px;">
+					<ons-col width="40px" style="text-align: center; line-height: 31px;">
+						<ons-icon icon="md-thumb-down"></ons-icon>
+					</ons-col>
+					<ons-col>
+						<ons-range style="width: 100%;" value="75"></ons-range>
+					</ons-col>
+					<ons-col width="40px" style="text-align: center; line-height: 31px;">
+						<ons-icon icon="md-thumb-up"></ons-icon>
+					</ons-col>
+				</ons-row>
+
+			<ons-list>
+				<ons-list-header>Select option to start</ons-list-header>
+				<ons-list-item>Link 1: Youtube - Custard tart</ons-list-item>
+				<ons-list-item>Link 2: Blog - Matcha Pudding</ons-list-item>
+				<ons-list-item>Link 3: Instagram - Berry cupcake</ons-list-item>
+			</ons-list>
+		</div>
+
+		<ons-button modifier="large--quiet" style="display: inline-block;">Maybe Later</ons-button>
+	</ons-card>
+
+	<style type="text/css">
+	.header-view {
+		width: 100%;
+		height: 20%;
+		text-align: center;
+		display: table;
+	}
+
+	.headericon {
+		vertical-align: middle !important;
+		font-size: 100px;
+		opacity: 1;
+		display: table-cell;
+	}
+
+	</style>
+
+<ons-bottom-toolbar>
+	<div class="tabbar">
+		<label class="tabbar__item">
+			<input type="radio" name="tabbar-a" checked="checked">
+			<button class="tabbar__button">
+				<i class="tabbar__icon ion-stop"></i>
+				<div class="tabbar__label">Home</div>
+			</button>
+		</label>
+
+		<label class="tabbar__item">
+			<input type="radio" name="tabbar-a">
+			<button class="tabbar__button">
+				<i class="tabbar__icon ion-record"></i>
+				<div class="tabbar__label">Explore</div>
+			</button>
+		</label>
+
+		<label class="tabbar__item">
+			<input type="radio" name="tabbar-a">
+			<button class="tabbar__button">
+				<i class="tabbar__icon ion-star"></i>
+				<div class="tabbar__label">Three</div>
+			</button>
+		</label>
+
+		<label class="tabbar__item">
+			<input type="radio" name="tabbar-a">
+			<button class="tabbar__button">
+				<i class="tabbar__icon ion-stop"></i>
+				<div class="tabbar__label">Four</div>
+			</button>
+		</label>
+
+	</div>
+</ons-bottom-toolbar>
+
+
+</ons-page>
+
+</body>
+
+</html>
