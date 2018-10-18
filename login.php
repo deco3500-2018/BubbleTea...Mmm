6301ad599c02de506edd88b0e61dacc464dca622
Kaira Wong <wongtsetse@gmail.com>
2018-10-18 10:45:57 +1000

    replaced old login page with new one. added css file to clean up style code in the php files

--- login.php
+++ login.php
@@ -72,97 +72,33 @@ if(isset($_POST['register_btn']))
 	<link rel="stylesheet" href="https://unpkg.com/onsenui/css/onsenui.css">
 	<link rel="stylesheet" href="https://unpkg.com/onsenui/css/onsen-css-components.min.css">
 	<script src="https://unpkg.com/onsenui/js/onsenui.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 
 <body>
 
-	<ons-page id="login-bg">
-
-		<style type="text/css">
-		#login-bg .page__background {
-			background-image: url('img/grad.png');
-			object-fit: cover;
-			background-repeat: no-repeat;
-		}
-
-		#textclr{
-			color: white;
-		}
-
-	</style>
-
-	<!-- Navbar -->
-	<ons-toolbar class="toolbar toolbar--transparent">
-		<div class="toolbar__right">
-			<span class="toolbar-button">
-				<i class="ion-navicon" style="font-size:32px; vertical-align:-6px; margin-right: 10px;" id="textclr"></i>
-			</span>
-		</div>
-
-		<div class="toolbar__center" id="textclr" style="text-transform: bold;">
-			LOG IN
-		</div>
-
-		<!-- <div class="toolbar__right">
-			<span class="toolbar-button" id="textclr">Label</span>
-		</div> -->
-	</ons-toolbar>
+	<ons-page id="bg">
 
 	<!-- Login Section --> 
-	<form method="post" action="login.php">
+	<form method="post" action="login.php" style="margin-top: 50%;">
 		<div style="text-align: center;">
-			<h1 id="textclr" style="text-align: center; margin-top: 10%;">Log In</h1>
+			<h1 id="text-bold" style="text-align: center; margin-top: 10%;">IN THE MOMENT</h1>
 			<br>
-			<p>
-				<ons-input input-id="textclr" class="textInput form-control" id="name" modifier="underbar" placeholder="Username" float type="text" required></ons-input>
-			</p>
 
-			<p>
-				<ons-input input-id="textclr" id="password" modifier="underbar" placeholder="Password" float type="text" required class="textInput form-control"></ons-input>
-			</p>
+			<ons-list style="max-width: 80%; margin: 10%;">
+				<ons-list-item modifier="noborder">
+					<ons-input placeholder="What's your name? "></ons-input>
+				</ons-list-item>
+			</ons-list>
 
+			<section style="max-width: 80%; margin: 10%;">
+				<ons-button modifier="button large--cta">
+					<ons-input input-id="textclr" type="submit" name="login_btn" value="START">
+				</ons-button>
 
-			<section style="padding: 8px;">
-				<ons-button class="button button--cta">
-					<ons-input input-id="textclr" type="submit" name="login_btn" value="Login">
-					</ons-button>
-					<br><br>
-					<small class="text-muted">Don't have an account?</small>
-					<br><br>
-					<ons-button class="button button--cta">
-						<ons-input input-id="textclr" style="display: inline-block; color: white;" type="button" value="Register" name="register_btn" onclick="location.href='register.php'">
-						</ons-button>
-						<br>
-						<br>
-					</section>
-
-
-					<p>OR<br><br>
-
-						<?php
-						// Facebook API
-						require_once( 'vendor/autoload.php' );
-						// Initialize the Facebook PHP SDK v5.
-						$fb = new Facebook\Facebook([
-							'app_id'                => '736882723323708',
-							'app_secret'            => '67e1b3111e10c972cab13cc1564c95fb',
-							'default_graph_version' => 'v2.10',
-						]);
-
-						$helper = $fb->getRedirectLoginHelper();
-
-			$permissions = ['public_profile','email','user_location','user_likes']; // Optional permissions
-			$loginUrl = $helper->getLoginUrl('https://s4436053-inthemoment.uqcloud.net/fb-callback.php', $permissions);
-
-
-			echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
-			?>
-		</p>
-
-	</div>
-</div>
-
-</form>
+			</section>
+		</div>
+	</form>
 
 </ons-page>
 
