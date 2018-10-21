9fa1228ef1953c1236b826c24f41f512684cdbbe
Kaira Wong <wongtsetse@gmail.com>
2018-10-21 11:19:37 +1000

    styled the time selection page, set input to number only with range between 5 - 75

--- timeSelection.php
+++ timeSelection.php
@@ -18,14 +18,26 @@ include "error.php";
 <body>
 
 	<ons-page id="bg">
-		<h1 class="title" id="textclr">How much free time do you have now?</h1>
-		<div>
-			<!-- dynamically create the checkbox base on interest retrieved from the database-->
+		<div style="text-align: center; margin-top: 50%;">
+			<h1 style="margin: 10%;" class="title" id="textclr">How much free time do you have now?</h1>
+			<div>
+				<!-- dynamically create the checkbox base on interest retrieved from the database-->
 
-			<form name="interestForm" action="categorySelection.php" method="post">
-				<input type="text" name="time">
-				<input type="submit" value="Submit">
-			</form>
+				<form name="interestForm" action="categorySelection.php" method="post">
+					<span class="list-item__subtitle" style="display: block;" id="textclr">* Please enter number of minutes</span>
+					<ons-list style="max-width: 40%; margin-right: auto; margin-left: auto; margin-top: 20px; margin-bottom: 20px;">
+						<ons-list-item modifier="noborder">
+							<ons-input type="number" name="time" min="5" max="75"></ons-input>
+						</ons-list-item>	
+					</ons-list>
+					
+
+
+					<ons-button modifier="cta">
+						<ons-input type="submit" value="Submit" input-id="textclr"></ons-input>
+					</ons-button>
+				</form>
+			</div>
 		</div>
 	</ons-page>
 
