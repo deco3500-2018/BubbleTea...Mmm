c633f6ffd721f789a4cb29ff2746767b9129eb1b
Kaira Wong <wongtsetse@gmail.com>
2018-10-22 11:24:44 +1000

    header now has back button, footer bar icons have been updated so its more accurate

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 		<label class="tabbar__item" onclick="location.href='index.php'">
 			<input type="radio" name="tabbar-a">
 			<button class="tabbar__button">
-				<i class="tabbar__icon ion-stop"></i>
+				<i class="tabbar__icon ion-home"></i>
 				<div class="tabbar__label">Home</div>
 			</button>
 		</label>
@@ -11,7 +11,7 @@
 		<label class="tabbar__item" onclick="location.href='timeSelection.php'">
 			<input type="radio" name="tabbar-a" checked="checked">
 			<button class="tabbar__button">
-				<i class="tabbar__icon ion-record"></i>
+				<i class="tabbar__icon ion-play"></i>
 				<div class="tabbar__label">Start</div>
 			</button>
 		</label>
@@ -19,7 +19,7 @@
 		<label class="tabbar__item" onclick="location.href='index.php'">
 			<input type="radio" name="tabbar-a">
 			<button class="tabbar__button">
-				<i class="tabbar__icon ion-star"></i>
+				<i class="tabbar__icon ion-compass"></i>
 				<div class="tabbar__label">Explore</div>
 			</button>
 		</label>
--- header.php
+++ header.php
@@ -1,7 +1,8 @@
 <ons-toolbar class="toolbar toolbar--transparent">
-	<div class="toolbar__right">
-		<span class="toolbar-button">
-			<i class="ion-navicon" style="font-size:32px; vertical-align:-6px; margin-right: 10px; <?php if (isset($white)){echo "";}else{echo "color: white;";}?>"></i>
+	<div class="toolbar__left">
+		<span class="toolbar-button" style="<?php if (isset($white)){echo "";}else{echo "color: white;";}?>">
+			<ons-back-button onclick="window.history.back();" style="color: white;">Back</ons-back-button>
+			<!-- <i class="ion-navicon" style="font-size:32px; vertical-align:-6px; margin-right: 10px; <?php if (isset($white)){echo "";}else{echo "color: white;";}?>"></i> -->
 		</span>
 	</div>
 	
@@ -9,7 +10,7 @@
 		<?php echo $pageName ?>
 	</div>
 	
-	<div class="toolbar__left">
+	<div class="toolbar__right">
 		<ons-toolbar-button icon="md-face" style="margin-left: 10px; color: white;"></ons-toolbar-button>
 	</div>
 </ons-toolbar>
